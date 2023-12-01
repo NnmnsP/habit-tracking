5d37fa0012677244b7b2be749e614282dfe8f7f3
Nanmanas Poonyapat <38101663+NnmnsP@users.noreply.github.com>
2023-12-01 10:47:08 +0700

    Update Firebase plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,10 @@ plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.kotlinAndroid) apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    // Add the dependency for the Crashlytics Gradle plugin
+    id("com.google.firebase.crashlytics") version "2.9.9" apply false
 }
 buildscript {
     repositories {
